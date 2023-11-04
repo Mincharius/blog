6c3305a0538ce042afb2c175d92739248a5982e0
MuMu0502 <phanchau0502@gmail.com>
2023-11-04 22:04:43 +0700

    done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,7 +6,7 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, It's Mincharius!</b> Discover my stories and creative ideas.
+        <b>Hey, It&apos;s Mincharius!</b> Discover my stories and creative ideas.
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
